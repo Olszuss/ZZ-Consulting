ef1c5ea9b20153a7529698335fce2fb68f9bbeb2
Adam Olszewski <adamolszewski96@gmail.com>
2023-10-30 20:39:16 +0100

    reorganizing navigation and changing styles

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(express.static("public"));
-// sendFile will go here
+
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
+
 app.get('/contact', function(req, res) {
   res.sendFile(path.join(__dirname, '/contact.html'));
 });
+
+
+
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
